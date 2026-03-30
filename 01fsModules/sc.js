ab91fa7b473a1cc1f32ff70032706dbe4595398b
Saurav Kumar <saurav@gmail.com>
2026-03-30 10:43:28 +0530

    fsModules

---
+++ 01fsModules/sc.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+
+// To create files----------------------------------------
+
+// fs.writeFile("hey.txt", "hello", function (err) {
+//     if (err) console.error(err)
+//     else console.log('done');
+// })
+
+// To append text in  file---------------------------------
+
+// fs.appendFile("hey.txt", " how are you", function(err){
+//     if(err) console.error(err)
+//         else console.log('done')
+// })
+
+// To rename file-------------------------------------------
+// fs.rename("hey.txt", "saurav.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("renamed");
+// })
+
+
+// To Copy File---------------------------------------------
+// fs.copyFile("./saurav.txt", "./abd/chacha.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("copy ho gaya");
+// })
+
+// To Remove or Unlink File-------------------------------------------
+// fs.unlink("./abd/chacha.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("removed");
+// })
+// So, there was a file inside abd folder named chacha.txt has been removed
+
+// To Remove Folder--------------------------------------------------------
+// fs.rm("abd", { recursive: true }, function (err) {
+//     if (err) console.log(err);
+//     else console.log("Folder Removed")
+// })
+// So, there was a folder named abd has been removed
+
+//-----------------------------Topics to stydu--------------------------
+// To Create Folder, To read Folder, To read File (important)
+
