300441a249ad9ff88538e53f1fb536ff1fc5b81e
Saurav Kumar <saurav@gmail.com>
2026-03-30 12:33:25 +0530

    middleware and error handlings

--- 02Express/sc.js
+++ 02Express/sc.js
@@ -7,8 +7,35 @@ const express = require("express")
 
 const app = express()
 
+// To make data readable we use middleware (sessions and cookies)--------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Middleware------------------------------------------------------
+app.use(function(req, res, next){
+    console.log("This is the msg")
+    next()
+})
+
 app.get("/", function(req, res){
     res.send("Hello Saurav! How are you")
+})
+
+//Routing----------------------------------------------------------
+app.get("/city", function(req, res){
+    res.send("I live in Patna")
+})
+
+//Error handling---------------------------------------------------
+app.use(function(err, req, res, next){
+    console.log(err.stack)
+    res.status(500).send
+    ("something broke")
+})
+
+// To Handle Error--------------------------------------------------
+app.get("./profile", function(req, res, next){
+    return next(new Error("Not implemented"))
 })
 
 app.listen(3000)
